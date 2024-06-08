09a8dc94b24c080bba489ee61f30a93998d5d78c
creativeden <eurodeny@gmail.com>
2024-06-08 17:46:14 +0700

    Lesson - 45

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import { Htag } from "@/components";
+import { Button, Htag } from "@/components";
 
 export default function Home() {
     return (
         <>
             <Htag tag='h1'>Text</Htag>
+            <Button appearance='primary'>Primary Button</Button>
+            <Button appearance='ghost'>Ghost Button</Button>
         </>
     )
 }
