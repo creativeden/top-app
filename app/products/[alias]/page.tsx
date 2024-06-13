3cd72eb65572ebc81af51cf3009ad2b2d5e31c35
creativeden <eurodeny@gmail.com>
2024-06-13 18:03:29 +0700

    Lesson - 82

--- app/products/[alias]/page.tsx
+++ app/products/[alias]/page.tsx
@@ -1,9 +1,15 @@
+import { getMenu } from "@/api/menu";
 import { getPage } from "@/api/page";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 export const metadata: Metadata = {
     title: 'Page',
+}
+
+export async function generateStaticParams() {
+    const menu = await getMenu(0)
+    return menu.flatMap(item => item.pages.map(page => ({ alias: page.alias })))
 }
 
 export default async function PageProducts({ params }: { params: { alias: string } }) {
